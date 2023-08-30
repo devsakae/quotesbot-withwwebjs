1842fcd1d600302e429c151310ea971b0a064b1e
Rodrigo Sakae <devsakae@gmail.com>
2023-08-30 10:44:53 -0300

    chore: o teste subiu junto

--- teste.js
+++
@@ -1,5 +0,0 @@
-const objeto = [{ name: 'Rodrigo', gols: 10 }, { name: 'Binho', gols: 0 }, { name: 'Binho', gols: 2 }, { name: 'Vitus', gols: 8 }]
-
-console.log(objeto.sort((a, b) => a.gols + b.gols))
-
-console.log(objeto.sort((a, b) => b.gols - a.gols))
